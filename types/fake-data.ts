9b0efc059c42e7235735f995df2da85cb75231b8
e.zheltukhin <e.zheltukhin@kt-team.de>
2025-05-30 20:08:08 +0500

    Add prisma-generator-fake-data

---
+++ types/fake-data.ts
@@ -0,0 +1,70 @@
+import { faker } from '@faker-js/faker';
+
+export function fakeWorkspace() {
+  return {
+    title: faker.lorem.words(5),
+    slug: faker.lorem.words(5),
+    summary: undefined,
+    content: undefined,
+    updatedAt: faker.date.anytime(),
+    deletedAt: undefined,
+  };
+}
+export function fakeWorkspaceComplete() {
+  return {
+    id: faker.string.uuid(),
+    title: faker.lorem.words(5),
+    slug: faker.lorem.words(5),
+    summary: undefined,
+    content: undefined,
+    createdAt: new Date(),
+    updatedAt: faker.date.anytime(),
+    deletedAt: undefined,
+  };
+}
+export function fakeCategory() {
+  return {
+    title: faker.lorem.words(5),
+    slug: faker.lorem.words(5),
+    summary: undefined,
+    content: undefined,
+    updatedAt: faker.date.anytime(),
+    deletedAt: undefined,
+  };
+}
+export function fakeCategoryComplete() {
+  return {
+    id: faker.string.uuid(),
+    title: faker.lorem.words(5),
+    slug: faker.lorem.words(5),
+    summary: undefined,
+    content: undefined,
+    createdAt: new Date(),
+    updatedAt: faker.date.anytime(),
+    deletedAt: undefined,
+    workplaceId: faker.string.uuid(),
+  };
+}
+export function fakeArticle() {
+  return {
+    title: faker.lorem.words(5),
+    slug: faker.lorem.words(5),
+    summary: undefined,
+    content: undefined,
+    updatedAt: faker.date.anytime(),
+    deletedAt: undefined,
+  };
+}
+export function fakeArticleComplete() {
+  return {
+    id: faker.string.uuid(),
+    title: faker.lorem.words(5),
+    slug: faker.lorem.words(5),
+    summary: undefined,
+    content: undefined,
+    createdAt: new Date(),
+    updatedAt: faker.date.anytime(),
+    deletedAt: undefined,
+    categoryId: faker.string.uuid(),
+  };
+}
